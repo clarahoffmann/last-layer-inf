32454b29cacdc2c2812a9d2b60342099179bd368
Clara Hoffmann <claracharlotteh@web.de>
2025-08-11 18:36:16 +0200

    add shebang

--- src/run_models.sh
+++ src/run_models.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 
 poetry run python ./run_models.py --method lli_vi_closed_full_cov --outpath ./results --model_dims 1 100 100 100 50 1 --data synthetic
 poetry run python ./run_models.py --method mc_dropout --outpath ./results --model_dims 1 100 100 100 50 1 --data synthetic
