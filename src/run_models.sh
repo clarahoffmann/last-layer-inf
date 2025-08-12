16a1a137a3bdcaf3f3faa9e337dcbf5f3025e68c
Clara Hoffmann <claracharlotteh@web.de>
2025-08-12 11:10:36 +0200

    add gibbs synth

--- src/run_models.sh
+++ src/run_models.sh
@@ -2,3 +2,5 @@
 
 poetry run python ./run_models.py --method lli_vi_closed_full_cov --outpath ./results --model_dims 1 100 100 100 50 1 --data synthetic
 poetry run python ./run_models.py --method mc_dropout --outpath ./results --model_dims 1 100 100 100 50 1 --data synthetic
+poetry run python ./run_models.py --method bnn --outpath ./results --model_dims 1 100 100 100 50 1 --data synthetic
+poetry run python ./run_models.py --method lli_gibbs_ridge --outpath ./results --model_dims 1 100 100 100 50 1 --data synthetic
