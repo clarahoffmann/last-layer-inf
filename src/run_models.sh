1f7626c805f35b217fbc676aa5a019260431a38a
Clara Hoffmann <claracharlotteh@web.de>
2025-08-12 13:10:37 +0200

    add more methods

--- src/run_models.sh
+++ src/run_models.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-poetry run python ./run_models.py --method lli_vi_closed_full_cov --outpath ./results --model_dims 1 100 100 100 50 1 --data synthetic
+poetry run python ./run_models.py --method lli_closed_form --outpath ./results --model_dims 1 100 100 100 50 1 --data synthetic
 poetry run python ./run_models.py --method mc_dropout --outpath ./results --model_dims 1 100 100 100 50 1 --data synthetic
 poetry run python ./run_models.py --method bnn --outpath ./results --model_dims 1 100 100 100 50 1 --data synthetic
 poetry run python ./run_models.py --method lli_gibbs_ridge --outpath ./results --model_dims 1 100 100 100 50 1 --data synthetic
+
+poetry run python ./run_models.py --method lli_vi_closed_form --outpath ./results --model_dims 1 100 100 100 50 1 --data synthetic
+poetry run python ./run_models.py --method lli_vi_ridge --outpath ./results --model_dims 1 100 100 100 50 1 --data synthetic
+poetry run python ./run_models.py --method lli_vi_horseshoe --outpath ./results --model_dims 1 100 100 100 50 1 --data synthetic
