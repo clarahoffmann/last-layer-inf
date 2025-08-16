1ef6579cdf35e216cefaa614525efc16125ca982
Clara Hoffmann <claracharlotteh@web.de>
2025-08-16 14:09:30 +0200

    add concrete data models

--- src/run_models.sh
+++ src/run_models.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# synthetic data
+# run lli_closed_form first to obtain the backbone for all LLI models and training data
 poetry run python ./run_models.py --method lli_closed_form --outpath ./results --model_dims 1 100 100 100 50 1 --data synthetic
 poetry run python ./run_models.py --method mc_dropout --outpath ./results --model_dims 1 100 100 100 50 1 --data synthetic
 poetry run python ./run_models.py --method bnn --outpath ./results --model_dims 1 100 100 100 50 1 --data synthetic
@@ -11,3 +13,17 @@ poetry run python ./run_models.py --method lli_vi_horseshoe --outpath ./results 
 
 poetry run python ./run_models.py --method lli_vi_ridge_full_fac --outpath ./results --model_dims 1 100 100 100 50 1 --data synthetic
 poetry run python ./run_models.py --method lli_vi_horseshoe_full_fac --outpath ./results --model_dims 1 100 100 100 50 1 --data synthetic
+
+
+# concrete compressive strength
+poetry run python ./run_models.py --method lli_closed_form --outpath ./results --model_dims 8 200 200 100 50 1 --data concrete
+poetry run python ./run_models.py --method mc_dropout --outpath ./results --model_dims 8 200 200 100 50 1 --data concrete
+poetry run python ./run_models.py --method bnn --outpath ./results --model_dims 8 200 200 100 50 1 --data concrete
+poetry run python ./run_models.py --method lli_gibbs_ridge --outpath ./results --model_dims 8 200 200 100 50 1 --data concrete
+
+poetry run python ./run_models.py --method lli_vi_closed_form --outpath ./results --model_dims 8 200 200 100 50 1  --data concrete
+poetry run python ./run_models.py --method lli_vi_ridge --outpath ./results --model_dims 8 200 200 100 50 1 --data concrete
+poetry run python ./run_models.py --method lli_vi_horseshoe --outpath ./results --model_dims 8 200 200 100 50 1 --data concrete
+
+poetry run python ./run_models.py --method lli_vi_ridge_full_fac --outpath ./results --model_dims 8 200 200 100 50 1 --data concrete
+poetry run python ./run_models.py --method lli_vi_horseshoe_full_fac --outpath ./results --model_dims 8 200 200 100 50 1 --data concrete
